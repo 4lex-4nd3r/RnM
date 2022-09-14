1fceb1b5c7ca2487fbefcea29571095aa53fa3b2
Alexander <abrossimov@yandex.ru>
2022-09-14 09:28:30 +0300

    Initial Commit

---
+++ R'n'M-test-v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  R'n'M-test-v1
+//
+//  Created by Александр on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
